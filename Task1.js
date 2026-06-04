8d9c97f477dcf500b0c02d3b68536e8dfd8bba6c
SoundarRP <soundarrp406@gmail.com>
2026-06-04 17:14:46 +0530

    first commit

---
+++ Task1.js
@@ -0,0 +1,79 @@
+// Task 1: Variable Practice
+
+let studentName ="Soundar P";
+let studentAge = 24;
+let studentCourse = "Full Stack Developer";
+
+console.log(studentName);
+console.log(studentAge);
+console.log(studentCourse);
+
+// Task 2: Var, Let, Const
+
+var company = "Stackly";
+let salary = 20000;
+const country = "India";
+console.log(company);
+console.log(salary);
+
+company = "Google";
+salary = 30000;
+console.log(company);
+console.log(salary);
+console.log(country);
+
+//Task 3:
+
+let userName =prompt("What is your name?");
+console.log("Welcome " + userName);
+
+//Task 4: Confirm Box
+
+let a = confirm("Do you know JavaScript?");
+console.log(a);
+
+//Task 5: Student Information
+
+let StudentName = prompt("Enter Student Name");
+let StudentAge = prompt("Enter Student Age");
+let StudentCity = prompt("Enter Student City");
+
+console.log("Name : " + StudentName);
+console.log("Age : " + StudentAge);
+console.log("City : " + StudentCity);
+
+//Task 6: Alert Message
+
+let course = "JavaScript";
+alert("Welcome to "+ course);
+
+//Task 7: Document Write
+
+document.writeln("Hello Student <br> Welcome to JavaScript");
+
+//Task 8: Favorite Things
+
+var FavoriteFood = "Chicken Biryani";
+var FavoriteColor = "Black";
+var FavoriteMovie = "Leo";
+console.log("Food : "+FavoriteFood);
+console.log("Color : "+ FavoriteColor);
+console.log("Movie: "+ FavoriteMovie);
+
+//Task 9: Mini Bio Data
+
+let name ="soundar RP";
+let age = 24;
+let phone = "7810074766";
+let email = "soundarrp456@gmail.com";
+console.log(name);
+console.log(age);
+console.log(phone);
+console.log(email);
+
+//Task 10 (Challenge)
+
+let Name = prompt("Enter Your Name");
+let Course = prompt("Enter Your Course");
+
+alert("Hello " + Name + ", Welcome to " + Course);
